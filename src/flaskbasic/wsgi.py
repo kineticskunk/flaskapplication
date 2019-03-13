6c56a68d37bb4b3750adb9346184c034e2e0efa7
nonzulu kala <nonzulu@kineticskunk.com>
2019-03-13 13:03:59 +0200

    adding h tag

--- src/flaskbasic/wsgi.py
+++ src/flaskbasic/wsgi.py
@@ -5,7 +5,7 @@ from src.flaskbasic.form import StudentForm
 from src.flaskbasic.models import Student
 import sys
 import logging
-
+H1
 # logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
 _logger_adding = logging.getLogger('Adding results')
 _logger_getting = logging.getLogger('Get results')
