5c219a4b967a5197f4e0c0daaa16b0d0effce343
Lwando92 <lwando@kineticskunk.com>
2019-03-22 13:18:52 +0200

    new tests

--- src/flaskbasic/models.py
+++ src/flaskbasic/models.py
@@ -3,11 +3,34 @@ from src.flaskbasic import db, application
 
 # how the data is structured in the database
 class Student(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50), nullable= False)
-  physics = db.Column(db.Integer)
-  maths = db.Column(db.Integer)
-  chemistry = db.Column(db.Integer)
 
-  def __repr__(self):
-    return "Student('{self.id}', '{self.name}',{self.physics}',{self.maths}',{self.chemistry}')"
+    __tablename__ = 'Student'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable= False)
+    physics = db.Column(db.Integer)
+    maths = db.Column(db.Integer)
+    chemistry = db.Column(db.Integer)
+
+    def __init__(self,name,physics,maths,chemistry):
+        self.name = name
+        self.physics = physics
+        self.maths = maths
+        self.chemistry = chemistry
+
+    def get_id(self):
+        return str(self.id)
+
+    def get_name(self):
+        return str(self.name)
+
+    def get_physics(self):
+       return str(self.physics)
+
+    def get_maths(self):
+        return str(self.maths)
+
+    def get_chemistry(self):
+        return str(self.chemistry)
+
+    def __repr__(self):
+        return "Student('{self.id}', '{self.name}',{self.physics}',{self.maths}',{self.chemistry}')"
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,50 @@
 """
 
 # import pytest
+import pytest
+from src.flaskbasic import create_app, db
+from src.flaskbasic.models import Student
+
+
+@pytest.fixture(scope='module')
+def new_student():
+    student = Student('Lwando', 10, 20, 30)
+    return student
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app('flask_test.cfg')
+
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = flask_app.test_client()
+
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+
+    yield testing_client  # this is where the testing happens!
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='module')
+def init_database():
+    # Create the database and the database table
+    db.create_all()
+
+    # Insert user data
+    student1 = Student('Lwando', 10, 20, 30)
+    student2 = Student('Lihle', 100, 200, 300)
+    # user1 = User(email='patkennedy79@gmail.com', plaintext_password='FlaskIsAwesome')
+    # user2 = User(email='kennedyfamilyrecipes@gmail.com', plaintext_password='PaSsWoRd')
+    db.session.add(student1)
+    db.session.add(student2)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield db  # this is where the testing happens!
+
+    db.drop_all()
