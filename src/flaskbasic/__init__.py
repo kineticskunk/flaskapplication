62459130230f96756e8f8455d723c135cf8641d9
Lwando92 <lwando@kineticskunk.com>
2019-03-28 12:35:57 +0200

    fixed tables error

--- src/flaskbasic/__init__.py
+++ src/flaskbasic/__init__.py
@@ -3,6 +3,9 @@ from pkg_resources import get_distribution, DistributionNotFound
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'secret'
@@ -10,6 +13,10 @@ application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 application.config['DEBUG']= True
 db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login = LoginManager(application)
+login.login_view = "users.login"
+
 
 def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
--- src/flaskbasic/wsgi.py
+++ src/flaskbasic/wsgi.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template, redirect, url_for,request, jsonify, abort,request,flash
 from flask_sqlalchemy import SQLAlchemy
 from src.flaskbasic import *
-from src.flaskbasic.form import StudentForm, LoginForm
-from src.flaskbasic.models import Student, User
+from src.flaskbasic.form import StudentForm, RegisterForm, LoginForm
+from src.flaskbasic.models import Student, Users
+from flask_login import login_user, current_user, login_required, logout_user
+
 import sys
 import logging
+
 
 # logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
 _logger_adding = logging.getLogger('Adding results')
@@ -17,7 +20,7 @@ _logger_delete = logging.getLogger('Delete results')
 
 
 # add student marks
-@application.route('/add_results', methods=['GET','POST'])
+@application.route('/', methods=['GET','POST'])
 def add_results():
     form = StudentForm()
     _logger_adding.warning("Inside Add Results function")
@@ -83,16 +86,46 @@ def delete_post(student_id):
       db.session.commit()
     return redirect(url_for('get_results'))
 
-@application.route("/", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-            user = User(email=form.email.data, password=form.password.data)
-            db.session.add(user)
-            db.session.commit()
-    # return redirect(url_for("add_results"))
+@application.route('/register', methods=['GET', 'POST'])
+def register():
+      # If the User is already logged in, don't allow them to try to register
+      if current_user.is_authenticated:
+          flash('Already registered!  Redirecting to your User Profile page...')
+          return redirect(url_for('add_results'))
 
-    return render_template('login.html',form=form)
+      form = RegisterForm()
+      if request.method == 'POST' and form.validate_on_submit():
+          new_user = Users(form.email.data, form.password.data)
+          new_user.authenticated = True
+          db.session.add(new_user)
+          db.session.commit()
+          login_user(new_user)
+          flash('Thanks for registering, {}!'.format(new_user.email))
+          return redirect(url_for('add_results'))
+      return render_template('register.html', form=form)
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    # If the User is already logged in, don't allow them to try to log in again
+    if current_user.is_authenticated:
+        flash('Already logged in!  Redirecting to your User Profile page...')
+        return redirect(url_for('users.profile'))
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = Users.query.filter_by(email=form.email.data).first()
+            if user and user.is_correct_password(form.password.data):
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user, remember=form.remember_me.data)
+                flash('Thanks for logging in, {}!'.format(current_user.email))
+                return redirect(url_for('add_results'))
+
+        flash('ERROR! Incorrect login credentials.')
+    return render_template('login.html', form=form)
 
 
 #
